c093075c50b2a73066f95d8f0d9d809a867617af
dubosqN <noe.dubosq.allende@gmail.com>
2020-10-05 23:27:09 +0200

    Accueil Modif

--- src/java/database/ChevalDAO.java
+++ src/java/database/ChevalDAO.java
@@ -32,7 +32,7 @@ public class ChevalDAO {
         try
         {
             //preparation de la requete     
-            requete=connection.prepareStatement("SELECT cheval.id, cheval.nom, cheval.sexe, cheval.prixDepart, cheval.SIRE, typecheval.libelle as Race, chevalPere.nom as pere, chevalMere.nom as mere, client.nom as nomVendeur "
+            requete=connection.prepareStatement("SELECT cheval.id, cheval.nom, cheval.sexe, cheval.prixDepart, cheval.SIRE, cheval.img_url, typecheval.libelle as Race, chevalPere.nom as pere, chevalMere.nom as mere, client.nom as nomVendeur "
                     + "FROM client, cheval, typecheval, cheval chevalPere, cheval chevalMere "
                     + "WHERE cheval.id_typeCheval = typecheval.id AND cheval.id_pere = chevalPere.id AND cheval.id_mere = chevalMere.id AND cheval.id_client = client.id "
                     + "ORDER BY cheval.id;");
@@ -47,6 +47,7 @@ public class ChevalDAO {
                 unCheval.setSexe(rs.getString("sexe"));
                 unCheval.setPrixDepart(rs.getInt("prixDepart"));
                 unCheval.setSIRE(rs.getString("SIRE"));
+                unCheval.setImg_url(rs.getString("img_url"));
                
                 Cheval unChevalPere = new Cheval();
                 unChevalPere.setNom(rs.getString("pere"));
